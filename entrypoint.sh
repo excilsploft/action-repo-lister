866fb4cffb9886cb09a3c3efa34d9ab194b34d11
kyl10863 <kylejones@esri.com>
2020-07-28 14:17:12 -0700

    unzip either way

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,13 @@
 BIN_DOWNLOAD=linux-amd64-repol.zip
 if [ ! -e $BIN_DOWNLOAD ]
 then
+	echo "downloading repol"
 	wget --no-check-certificate --content-disposition  https://github.com/excilsploft/repo-lister/releases/download/v0.0.3/${BIN_DOWNLOAD}
-	unzip ${BIN_DOWNLOAD}
+else 
+	echo "found ${BIN_DOWNLOAD}"
+	ls -lah ${BIN_DOWLNLOAD}
 fi
-pwd
+unzip ${BIN_DOWNLOAD}
+ls -lah 
 repo_yaml=$(repol)
 echo "::set-output name=repo_yaml::$repo_yaml"
