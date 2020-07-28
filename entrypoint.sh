93cdc744c3de6454be75ab27f2715ab1ff7a4f65
kyl10863 <kylejones@esri.com>
2020-07-28 14:08:54 -0700

    repol location

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ then
 	wget --no-check-certificate --content-disposition  https://github.com/excilsploft/repo-lister/releases/download/v0.0.3/${BIN_DOWNLOAD}
 	unzip ${BIN_DOWNLOAD}
 fi
-
-repo_yaml=$(/repol)
+pwd 
+repo_yaml=$(repol)
 echo "::set-output name=repo_yaml::$repo_yaml"
