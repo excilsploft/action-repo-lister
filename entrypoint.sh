79ce839298c9589389a732b7f69d0a32f458453e
kyl10863 <kylejones@esri.com>
2020-07-28 14:12:19 -0700

    bad bracket

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -l
 
 BIN_DOWNLOAD=linux-amd64-repol.zip
-if [ ! -e $BIN_DOWNLOAD]
+if [ ! -e $BIN_DOWNLOAD ]
 then
 	wget --no-check-certificate --content-disposition  https://github.com/excilsploft/repo-lister/releases/download/v0.0.3/${BIN_DOWNLOAD}
 	unzip ${BIN_DOWNLOAD}
 fi
-pwd 
+pwd
 repo_yaml=$(repol)
 echo "::set-output name=repo_yaml::$repo_yaml"
